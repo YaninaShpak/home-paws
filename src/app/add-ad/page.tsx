ecfe52919dc6fb05930ce060c1277c2af5ab9564
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2025-10-22 21:33:23 +0500

    add pages routing

---
+++ src/app/add-ad/page.tsx
@@ -0,0 +1,11 @@
+const AddAdPage = () => {
+  return (
+    <div className="container">
+      <h1>
+        AddAdPage
+      </h1>
+    </div>
+  );
+};
+
+export default AddAdPage;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+const LoginPage = () => {
+  return (
+    <div className="container">
+      <h1>
+        LoginPage
+      </h1>
+    </div>
+  );
+};
+
+export default LoginPage;
